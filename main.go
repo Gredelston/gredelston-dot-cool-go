22664297db16ef9986f16d1c6dca6c058d5333c3
Greg Edelston <gredelston@gmail.com>
2020-07-07 13:41:09 -0600

    Render with header & footer templates

--- main.go
+++ main.go
@@ -9,15 +9,19 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+const (
+	// Generic template filepaths
+	headerTFP = "template/header.html"
+	footerTFP = "template/footer.html"
+
+	// Page-specific template filepaths
+	indexTFP = "template/index.html"
+)
+
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	t, err := template.ParseFiles("template/page.html")
-	if err != nil {
-		fmt.Errorf("Failed to parse files: %w", err)
-		return
-	}
-	data := map[string]string {"body": "Welcome to my website!"}
-	if err = t.Execute(w, data); err != nil {
-		fmt.Errorf("Failed to execute template: ", err)
+	t := template.Must(template.ParseFiles(indexTFP, headerTFP, footerTFP))
+	if err := t.Execute(w, map[string]string{"title": "Index"}); err != nil {
+		panic(err)
 	}
 }
 

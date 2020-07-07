ac08973bfc05acc50a6ca1f8ebdda578957c40fb
Greg Edelston <gredelston@gmail.com>
2020-07-07 13:52:32 -0600

    Create 'About Me' page

--- main.go
+++ main.go
@@ -15,13 +15,21 @@ const (
 	footerTFP = "template/footer.html"
 
 	// Page-specific template filepaths
+	aboutTFP = "template/about.html"
 	indexTFP = "template/index.html"
 )
 
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	t := template.Must(template.ParseFiles(indexTFP, headerTFP, footerTFP))
 	if err := t.Execute(w, map[string]string{"title": "Index"}); err != nil {
-		panic(err)
+		panic(fmt.Errorf("Executing template for / (index): ", err))
+	}
+}
+
+func About(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	t := template.Must(template.ParseFiles(aboutTFP, headerTFP, footerTFP))
+	if err := t.Execute(w, map[string]string{"title": "About Me"}); err != nil {
+		panic(fmt.Errorf("Executing template for /about: ", err))
 	}
 }
 
@@ -32,6 +40,7 @@ func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 func main() {
 	router := httprouter.New()
 	router.GET("/", Index)
+	router.GET("/about", About)
 	router.GET("/hello/:name", Hello)
 
 	log.Fatal(http.ListenAndServe(":8080", router))

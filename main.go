0fd85791dcddb4ed21a5344f5b4a4345fd44de7d
Greg Edelston <gredelston@gmail.com>
2020-07-14 15:25:05 -0600

    Detect whether blog dir/file exists

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 	"log"
+	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/julienschmidt/httprouter"
@@ -47,7 +50,7 @@ type BlogData struct {
 	Body  string
 }
 
-// Generic function to render a specified template (fp) with custom data.
+// renderPage generically render a specified template (fp) with custom data.
 func renderPage(fp string, w http.ResponseWriter, data PageData) {
 	if len(data.Navs) == 0 {
 		data.Navs = NewNavs()
@@ -56,7 +59,15 @@ func renderPage(fp string, w http.ResponseWriter, data PageData) {
 	if err := t.Execute(w, data); err != nil { panic(err) }
 }
 
-// GET method to route homepage.
+// renderError renders generic error text to the browser.
+func renderError(err error, w http.ResponseWriter) {
+	renderPage(textTFP, w, PageData{
+		Title: "Arrow'd!",
+		Body: fmt.Sprintf("%+v", err),
+	})
+}
+
+// Index defines a GET method to route homepage.
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	renderPage(indexTFP, w, PageData{
 		Title: "Home",
@@ -65,7 +76,7 @@ func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	})
 }
 
-// GET method to route "About Me" page.
+// About defines a GET method to route "About Me" page.
 func About(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	renderPage(textTFP, w, PageData{
 		Title: "About Me",
@@ -75,9 +86,34 @@ func About(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	})
 }
 
+// exists checks whether path is present on the local filesystem.
+func exists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil { return true, nil }
+	if os.IsNotExist(err) { return false, nil }
+	return false, err
+}
+
 // GET method to route blog pages.
-func Blog(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	title := "My Fake Blog Post"
+func Blog(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	slug := ps.ByName("slug")
+	blogDir := filepath.Join("static", "blog", slug)
+	if blogDirExists, err := exists(blogDir); err != nil {
+		renderError(fmt.Errorf("Finding blogdir for slug %q: %+v", slug, err), w)
+		return
+	} else if !blogDirExists {
+		renderError(fmt.Errorf("Did not find blogdir for slug %q", slug), w)
+		return
+	}
+	blogFile := filepath.Join(blogDir, "index.md")
+	if blogFileExists, err := exists(blogFile); err != nil {
+		renderError(fmt.Errorf("Finding blogfile for slug %q: %+v", slug, err), w)
+		return
+	} else if !blogFileExists {
+		renderError(fmt.Errorf("Did not find blogfile for slug %q", slug), w)
+		return
+	}
+	title := slug
 	date := time.Now()
 	tags := []string{"CoolTag1", "UncoolTagLambda"}
 	body := "Wow, what a cool blog post!"

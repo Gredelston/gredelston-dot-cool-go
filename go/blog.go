5e5fe15255f0fe9f9e8b245983109d28fd514a22
Greg Edelston <gredelston@gmail.com>
2020-07-19 11:23:52 -0600

    Functionalize and test allDirsWithin

--- go/blog.go
+++ go/blog.go
@@ -17,16 +17,27 @@ type BlogPost struct {
 	Tags   []string
 }
 
-func (s *Server) LoadBlogData() error {
-	blogRoot := s.BlogRoot()
-	dirs, err := ioutil.ReadDir(blogRoot)
+// allDirsWithin returns an array of directories within some dir parent.
+func allDirsWithin(parent string) ([]string, error) {
+	var childDirs []string
+	children, err := ioutil.ReadDir(parent)
 	if err != nil {
-		return fmt.Errorf("failed to read files from blog root: %+v", err)
+		return nil, fmt.Errorf("reading dir %s: %+v", parent, err)
 	}
+	for _, child := range children {
+		if child.IsDir() {
+			childDirs = append(childDirs, filepath.Join(parent, child.Name()))
+		}
+	}
+	return childDirs, nil
+}
+
+func (s *Server) LoadBlogData() error {
+	blogPostDirs, err := allDirsWithin(s.BlogRoot())
+	if err != nil { return err }
 	var posts []BlogPost
-	for _, dir := range dirs {
-		if !dir.IsDir() { continue }
-		indexPath := filepath.Join(blogRoot, dir.Name(), "index.md")
+	for _, dir := range blogPostDirs {
+		indexPath := filepath.Join(dir, "index.md")
 		fmt.Println(indexPath)
 		if exists, err := s.FullPathExists(indexPath); err != nil {
 			return fmt.Errorf("determining whether blog post %s has an index.md: %+v", dir, err)
@@ -36,7 +47,7 @@ func (s *Server) LoadBlogData() error {
 		posts = append(posts, BlogPost{
 			Body: "Hello!",
 			Date: time.Now(),
-			Slug: dir.Name(),
+			Slug: filepath.Base(dir),
 			Title: "My cool title!",
 		})
 	}
--- go/blog_test.go
+++ go/blog_test.go
@@ -1,6 +1,11 @@
 package main
 
-import "testing"
+import (
+	"io/ioutil"
+	"os"
+	"reflect"
+	"testing"
+)
 
 func TestBlogRootExists(t *testing.T) {
 	s, err := NewServer()
@@ -10,5 +15,49 @@ func TestBlogRootExists(t *testing.T) {
 		t.Error(err)
 	} else if !exists {
 		t.Errorf("Blog root %q not present on server", blogRoot)
+	}
+}
+
+func TestAllDirsWithin(t *testing.T) {
+	// Create parent dir
+	tempParentDir, err := ioutil.TempDir("/tmp", "temp-parent-dir")
+	if err != nil {
+		t.Fatal("Creating temporary parent dir: ", err)
+	}
+	defer os.RemoveAll(tempParentDir)
+
+	// Populate with the following structure:
+	// temp-parent-dir/
+	// | temp-child-dir-1/
+	// | | temp-grandchild-dir/
+	// | temp-child-dir-2/
+	// | temp-child-file
+	tempChildDir1, err := ioutil.TempDir(tempParentDir, "temp-child-dir-1")
+	if err != nil {
+		t.Fatal("Creating temporary child dir 1: ", err)
+	}
+	tempChildDir2, err := ioutil.TempDir(tempParentDir, "temp-child-dir-2")
+	if err != nil {
+		t.Fatal("Creating temporary child dir 2: ", err)
+	}
+	if _, err = ioutil.TempDir(tempChildDir1, "temp-grandchild-dir"); err != nil {
+		t.Fatal("Creating temporary grandchild dir: ", err)
+	}
+	if _, err = ioutil.TempFile(tempParentDir, "temp-child-file"); err != nil {
+		t.Fatal("Creating temporary child file: ", err)
+	}
+
+	childDirs, err := allDirsWithin(tempParentDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(childDirs) != 2 {
+		t.Errorf("Expected 2 child dirs, but got %d: %+v", len(childDirs), childDirs)
+	}
+	expectedChildDirs := []string{tempChildDir1, tempChildDir2}
+	expectedChildDirsReverse := []string{tempChildDir2, tempChildDir1}
+	if !reflect.DeepEqual(childDirs, expectedChildDirs) && !reflect.DeepEqual(childDirs, expectedChildDirsReverse) {
+		t.Errorf("Unexpected childDirs: got %+v; want %+v", childDirs, expectedChildDirs)
 	}
 }
